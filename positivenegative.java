5ed09b87088fb5ee6174471ef3acd0bbc8963d30
Hitesh <hiteshshirude2004@gmail.com>
2025-01-15 11:37:51 +0530

    new Problem

---
+++ positivenegative.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class positivenegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number");
+        int n = sc.nextInt();
+        if(n>0){
+            System.out.println("Number is Positive");
+        }else{
+            System.out.println("Number is Negative");
+        }
+    }
+}
