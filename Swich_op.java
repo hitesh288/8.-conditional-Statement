e5003f57913d45af57c5034adab8c7433abfc00e
Hitesh <hiteshshirude2004@gmail.com>
2025-01-15 06:18:14 +0530

    New Problems

---
+++ Swich_op.java
@@ -0,0 +1,20 @@
+public class Swich_op {
+    public static void main(String[] args) {
+        int Num = 1;
+        switch (Num) {
+            case 1:
+                System.out.println("You Are Choose Number 1");
+            break;
+            case 2:
+                System.out.println("You Are Choose Number 2");
+            break;
+            case 3:
+            System.out.println("You Are Choose Number 3");
+            break;
+        
+            default:
+            System.out.println("You Are Choose wrong Number");
+                break;
+        }
+    }
+}
---
+++ passfail.java
@@ -0,0 +1,7 @@
+public class passfail {
+    public static void main(String[] args) {
+        int marks=29;
+        String result=(marks>=36)?"pass":"fail";            //Ternary Operator Syntax
+        System.out.println(result);
+    }
+}
